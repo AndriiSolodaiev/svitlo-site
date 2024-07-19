af7382d080afaea3c6e9c8505b28b5d807ae7400
AndriiSolodaiev <andrii.solodaiev@gmail.com>
2024-07-19 21:40:50 +0300

    add orientation script

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -3,22 +3,49 @@ import { Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import { gsap, ScrollTrigger, CustomEase } from 'gsap/all';
-
-import '../modules/helpers/imgParallax';
+import device from 'current-device';
+// import '../modules/helpers/imgParallax';
 
 import { initSmoothScrolling } from '../modules/scroll/leniscroll';
 
 initSmoothScrolling();
 gsap.registerPlugin(ScrollTrigger, CustomEase);
-const calcOffset = 101 * 2 + 300 - 2 * 200 - 150 - 250 + 10;
-const offset = window.innerHeight + calcOffset; // формула обчислення числа:  (к-ть Непарних карток зліва) * 101 + (к-ть Парних карток зліва) * 300 - (к-ть Непарних карток справа) * 200 - (к-ть Парних карток справа) * 100 - (різниця карток справа і зліва)* (відступ між картками (10))
-const tl = gsap.timeline({
+const leftBlock = document.querySelector('.left-block');
+let calcOffset =
+  window.innerHeight < window.innerWidth
+    ? 101 * 2 + 300 - 2 * 200 - 150 - 250 + 10
+    : 401 * 2 + 600 - 2 * 500 - 450 - 550 + 10;
+let offset = window.innerHeight + calcOffset; // формула обчислення числа:  (к-ть Непарних карток зліва) * 101 + (к-ть Парних карток зліва) * 300 - (к-ть Непарних карток справа) * 200 - (к-ть Парних карток справа) * 100 - (різниця карток справа і зліва)* (відступ між картками (10))
+let tl = gsap.timeline({
   scrollTrigger: {
     trigger: '.projects',
     start: '100px 100px', // when the top of the trigger hits the top of the viewport
     end: 'bottom bottom', // end after scrolling 500px beyond the start
-    scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the
+    scrub: 0.3, // smooth scrubbing, takes 1 second to "catch up" to the
     // markers: true,
   },
 });
-tl.to('.left-block', { y: offset, duration: 0.2 });
+tl.fromTo('.left-block', { y: 0 }, { y: offset });
+window.addEventListener('resize', function() {
+  console.log('resizing');
+  tl.kill();
+  tl = null;
+  leftBlock.style.transform = '';
+  console.log(leftBlock);
+  calcOffset =
+    window.innerHeight < window.innerWidth
+      ? 101 * 2 + 300 - 2 * 200 - 150 - 250 + 10
+      : 401 * 2 + 600 - 2 * 500 - 450 - 550 + 10;
+  offset = window.innerHeight + calcOffset;
+  // console.log(offset);
+  tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.projects',
+      start: '100px 100px', // when the top of the trigger hits the top of the viewport
+      end: 'bottom bottom', // end after scrolling 500px beyond the start
+      scrub: 0.3, // smooth scrubbing, takes 1 second to "catch up" to the
+      // markers: true,
+    },
+  });
+  tl.fromTo('.left-block', { y: 0 }, { y: offset });
+});
--- src/assets/scripts/modules/header/header.js
+++ src/assets/scripts/modules/header/header.js
@@ -1,7 +1,6 @@
 const header = document.querySelector('.header-bg');
 
 window.addEventListener('scroll', function headerSquosh() {
-
   const scrollPosition = window.scrollY;
   if (scrollPosition > 20) {
     header.classList.add('scroll-down');
@@ -11,49 +10,64 @@ window.addEventListener('scroll', function headerSquosh() {
 });
 
 document.body.addEventListener('click', function(evt) {
-  
   const close = evt.target.closest('[data-call-us-modal-close]');
-  const form = evt.target.closest('[data-call-us-modal]');
+  // const form = evt.target.closest('[data-call-us-modal]');
   const btn = evt.target.closest('[data-call-us-btn]');
   const overflow = document.querySelector('[data-call-us__overflow]');
-  
-  const countryList = evt.target.closest('.iti__country-list');
+  const menuContainer = document.querySelector('.menu__container');
+  // const countryList = evt.target.closest('.iti__country-list');
 
-  const btnUp = evt.target.closest("[data-btn-up]");
+  const btnUp = evt.target.closest('[data-btn-up]');
 
   const btnMenuTarget = evt.target.closest('[data-menu-button]');
-  const btnMenu =document.querySelector('[data-menu]')
-  const menu =document.querySelector('[data-menu]')
- if (btnMenuTarget) {
-      menu.classList.toggle('hidden');
-      header.classList.toggle('menu-is-open');
-      return
+  // const btnMenu = document.querySelector('[data-menu]');
+  const menu = document.querySelector('[data-menu]');
+  if (btnMenuTarget) {
+    menu.classList.toggle('hidden');
+
+    header.classList.toggle('menu-is-open');
+    window.dispatchEvent(new Event('stop-scroll'));
+    if (!header.classList.contains('menu-is-open')) {
+      window.dispatchEvent(new Event('start-scroll'));
+    }
+    return;
   }
-  if(btnUp){
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+  if (evt.target === menuContainer) {
+    menu.classList.toggle('hidden');
+
+    header.classList.toggle('menu-is-open');
+    window.dispatchEvent(new Event('stop-scroll'));
+    if (!header.classList.contains('menu-is-open')) {
+      window.dispatchEvent(new Event('start-scroll'));
+    }
+    return;
+  }
+  if (btnUp) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
   if (btn) {
     if (overflow.classList.contains('hidden')) {
-      
+      window.dispatchEvent(new Event('stop-scroll'));
       return overflow.classList.remove('hidden');
     }
     return;
   }
   if (close) {
+    window.dispatchEvent(new Event('start-scroll'));
     return overflow.classList.add('hidden');
   }
-  if ( evt.target === overflow) {
+  if (evt.target === overflow) {
+    window.dispatchEvent(new Event('start-scroll'));
     return overflow.classList.add('hidden');
   }
 });
 
-
-const loader = document.querySelector(".loader-wrap");
+const loader = document.querySelector('.loader-wrap');
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.onload = function () {
-      window.setTimeout(() => {
-        loader.classList.add("loaded")
-      }, 1200)
-    }
-  })
+  window.onload = function() {
+    window.setTimeout(() => {
+      loader.classList.add('loaded');
+    }, 1200);
+  };
+});
